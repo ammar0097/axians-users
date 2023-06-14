0cd3ebb4cf6efc3887d081c5f6b8f418b8dd38d8
Ammar <ammarferchichi7@gmail.com>
2023-06-14 18:51:06 +0100

    update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,11 @@ const db = require("../models");
 
 exports.getAllUsers = async (req, res) => {
   const users = await db.User.findAll();
-  if (users) {
-    res.status(200).send(users);
-  } else {
+  if (!users) {
     res.status(500).send("No users found!");
-  }
+  } 
+  res.status(200).send(users);
+
 };
 
 exports.getUserById = async (req, res) => {
@@ -37,8 +37,31 @@ exports.createUser = async (req, res) => {
   }
 };
 
-exports.updateUser = (req, res) => {
-  // Logic to update a user
+exports.updateUser = async (req, res) => {
+ 
+  const id_params = req.params.id;
+  try{
+   
+    const user = await db.User.findByPk(id_params);
+    if(!user){
+      res.status(404).send("user not found");
+    }
+    
+    await db.User.update({
+      username : req.body.username,
+      firstName : req.body.firstName,
+      lastName : req.body.lastName,
+      email: req.body.email,
+      password : req.body.password
+    },{where:{
+       id :id_params
+    }});
+
+    res.status(200).send("user updated");
+
+  }catch(error){
+    res.status(500).send(error);
+  }
 };
 
 exports.deleteUser = async (req, res) => {
